53d124000d89f79b45969015b3c144eba81e2663
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-02-02 10:08:19 +0000

    Styling header and footer

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule
