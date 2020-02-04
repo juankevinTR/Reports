cada3af5cf37556885b4da4b62d06e1cc4842d9e
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-02-04 23:11:23 +0000

    Success message after creating a new client

--- angular/src/app/clients/form.component.ts
+++ angular/src/app/clients/form.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Client} from './client';
 import {ClientService} from './client.service';
 import {Router} from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -19,9 +20,11 @@ export class FormComponent implements OnInit {
   }
 
   public create(): void {
-    this.clientService.create(this.client).subscribe(
-      response => this.router.navigate(['/clients'])
-    );
+    this.clientService.create(this.client)
+      .subscribe(client => {
+          this.router.navigate(['/clients']);
+          Swal.fire('New Client', `Client ${client.name} created successfully`, 'success');
+        }
+      );
   }
-
 }
